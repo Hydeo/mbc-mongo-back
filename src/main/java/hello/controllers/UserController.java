fd431946236a1bdf4a727a656789962642fa9167
Hydeo <p.garciapelayo@gmail.com>
2018-07-14 14:58:22 +0200

    Post / Get user

--- src/main/java/hello/controllers/UserController.java
+++ src/main/java/hello/controllers/UserController.java
@@ -2,6 +2,9 @@ package hello.controllers;
 
 import hello.entity.User;
 import hello.repository.user.UserRepo;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,5 +18,21 @@ public class UserController {
     @RequestMapping(method = RequestMethod.GET, value="/getByIdentifier/{identifier}")
     public User getUserByIdentifier(@PathVariable String identifier){
         return userRepo.findByIdentifier(identifier);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value= "/signUp")
+    public User signUpUser(@RequestBody String json){
+
+        //TODO Validate Json to check if valid
+
+        JSONParser jp = new JSONParser();
+        try{
+            JSONObject jp_parsed = (JSONObject) jp.parse(json);
+            return userRepo.save(new User(jp_parsed.get("uid").toString(), jp_parsed.get("identifier").toString()));
+        }
+        catch(ParseException e){
+            //TODO See what kind of return we should do for errors
+            return null;
+        }
     }
 }
