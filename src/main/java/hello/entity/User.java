a0b60f2b73f7d715a5c15f1ee089aa70c5d0f638
Hydeo <p.garciapelayo@gmail.com>
2020-11-06 10:46:02 +0000

    User Migration

--- src/main/java/hello/entity/User.java
+++ src/main/java/hello/entity/User.java
@@ -1,30 +1,51 @@
 package hello.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"created_at"},
+        allowGetters = true)
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public String id;
 
-    public String uid;
+    @Column
+    @NotBlank
     public String identifier;
 
-    public User(){}
+    public User() {
+    }
 
-    public User(String UID, String identifier){
-        this.uid = UID;
+    public User(String UID, String identifier) {
+        this.id = UID;
         this.identifier = identifier;
     }
 
 
     @Override
-    public String toString(){
-        return String.format("User[uid = %s,identifier=%s",uid,identifier);
+    public String toString() {
+        return String.format("User[uid = %s,identifier=%s", id, identifier);
     }
 
     public String getUid() {
-        return uid;
+        return id;
     }
 
     public void setUid(String uid) {
-        this.uid = uid;
+        this.id = uid;
     }
 
     public String getIdentifier() {
--- src/main/java/hello/repository/user/UserRepo.java
+++ src/main/java/hello/repository/user/UserRepo.java
@@ -1,8 +1,9 @@
 package hello.repository.user;
 
 import hello.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepo extends MongoRepository<User,String> {
+public interface UserRepo extends JpaRepository<User,Long> {
     User findByIdentifier(String identifier);
 }
